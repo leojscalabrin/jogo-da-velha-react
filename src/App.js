d773e8a83d6079b0679b4fae5f412cfdcf05b99f
Leonardo Scalabrin <leojscalabrin@gmail.com>
2022-09-04 16:06:48 -0300

    checar vencedor

--- src/App.js
+++ src/App.js
@@ -1,30 +1,53 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-import './App.css';
+import "./App.css";
 
-import {Board} from "./components/Board"
+import { Board } from "./components/Board";
 
 function App() {
-  const [board, setBoard] = useBoard(Array(9).fill(null))
-  const [xPlaying, setXPlaying] = useState(true)
+  const WIN_CONDITIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xPlaying, setXPlaying] = useState(true);
 
   const handleBoxClick = (boxIdx) => {
     const updatedBoard = board.map((value, idx) => {
-      if(idx === boxIdx) {
+      if (idx === boxIdx) {
         return xPlaying === true ? "X" : "O";
       } else {
         return value;
       }
-    })
+    });
+
+    checkWinner(updatedBoard);
 
     setBoard(updatedBoard);
 
     setXPlaying(!xPlaying);
+  };
+
+  const checkWinner = (board) => {
+    for(let i = 0; i < WIN_CONDITIONS.length; i++) {
+      const [x,y,z] = WIN_CONDITIONS[i];
+
+      if (board[x] && board[x] === board[y] && board[y] === board[z]) {
+        return board[x];
+      }
+    }
   }
 
   return (
     <div className="App">
-      <Board board={board} onClick={handleBoxClick}/>
+      <Board board={board} onClick={handleBoxClick} />
     </div>
   );
 }
