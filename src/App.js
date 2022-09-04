e54a87aa9c9bba47d49e03ea31da1e38ca3c738b
Leonardo Scalabrin <leojscalabrin@gmail.com>
2022-09-04 10:28:06 -0300

    estilizando as caixas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
+import {Box} from "./components/Box"
+
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Box value="X" onClick={null}/>
     </div>
   );
 }
