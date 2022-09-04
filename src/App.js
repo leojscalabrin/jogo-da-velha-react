ef825523596be288b31bbcc78acd55a65d6fcee9
Leonardo Scalabrin <leojscalabrin@gmail.com>
2022-09-04 10:42:21 -0300

    grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 
-import {Box} from "./components/Box"
+import {Board} from "./components/Board"
 
 function App() {
+  const board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
   return (
     <div className="App">
-      <Box value="X" onClick={null}/>
+      <Board board={board} onClick={null}/>
     </div>
   );
 }
